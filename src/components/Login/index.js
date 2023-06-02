dd00e6af51d9794a23cf177f4dae24b94a23da31
nilesh <nileshpatil11115@gmail.com>
2023-06-02 12:50:20 +0000

    first commit

---
+++ src/components/Login/index.js
@@ -0,0 +1,124 @@
+/* eslint-disable prettier/prettier */
+
+import {Component} from 'react'
+import Cookies from 'js-cookie'
+import './index.css'
+
+export default class Login extends Component {
+  state = {
+    username: '',
+    password: '',
+    errorMsg: '',
+    isErrorShown: false,
+  }
+
+  onFailedSubmit = errorMsg => {
+    this.setState({errorMsg, isErrorShown: true})
+  }
+
+  onSuccessSubmit = jwtToken => {
+    const {history} = this.props
+    Cookies.set('jwt_token', jwtToken, {expires: 300})
+    history.replace('/')
+    console.log(jwtToken)
+  }
+
+  onFormSubmit = async event => {
+    event.preventDefault()
+    const url = 'https://apis.ccbp.in/login'
+    const {username, password} = this.state
+    const userDetails = {username, password}
+    const options = {
+      method: 'POST',
+      body: JSON.stringify(userDetails),
+    }
+
+    const response = await fetch(url, options)
+    const data = await response.json()
+    console.log(data)
+    if (response.ok === true) {
+      this.onSuccessSubmit(data.jwt_token)
+    } else {
+      this.onFailedSubmit(data.error_msg)
+    }
+  }
+
+  onChangeUsername = event => {
+    this.setState({username: event.target.value})
+  }
+
+  onChangePassword = event => {
+    this.setState({password: event.target.value})
+  }
+
+  renderUsernameContainer = () => {
+    const {username} = this.state
+
+    return (
+      <>
+        <label htmlFor="usernameInput" className="credential-label">
+          USERNAME
+        </label>
+        <input
+          className="credentials-input "
+          id="usernameInput"
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={this.onChangeUsername}
+        />
+      </>
+    )
+  }
+
+  renderPasswordContainer = () => {
+    const {password} = this.state
+
+    return (
+      <>
+        <label htmlFor="passwordInput" className="credential-label">
+          PASSWORD
+        </label>
+        <input
+          className="credentials-input"
+          id="passwordInput"
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={this.onChangePassword}
+        />
+      </>
+    )
+  }
+
+  render() {
+    const {password, username, isErrorShown, errorMsg} = this.state
+
+    // console.log(username, password)
+    return (
+      <div className="login-container">
+        <div className="login-card-container">
+        <div className='login-logo-container'>
+          <img
+            alt="website-logo"
+            className="login-card-logo"
+            src="https://assets.ccbp.in/frontend/react-js/logo-img.png"
+          />
+</div>
+          <form className="form-container" onSubmit={this.onFormSubmit}>
+            <div className="credential-container">
+              {this.renderUsernameContainer()}
+            </div>
+            <div className="credential-container">
+              {this.renderPasswordContainer()}
+            </div>
+            <button className="login-btn" type="submit">
+              Login
+            </button>
+            {isErrorShown ? <p className='error-msg'>{`* ${errorMsg}`}</p> : null}
+          </form>
+        </div>
+      </div>
+    )
+  }
+}
